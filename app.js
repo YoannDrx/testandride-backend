05a777f547776351deeb2f471bf3d6cfdc2e1e58
Your name <Your email>
2023-03-03 09:26:43 +0100

    test route feedback

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var feedbacksRouter = require('./routes/feedbacks');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/feedbacks', feedbacksRouter);
 
 module.exports = app;
